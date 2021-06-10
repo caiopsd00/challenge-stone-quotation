26814740abefb704996b04aeaf63f729743e240c
caiopsd00 <caiopsd00@gmail.com>
2021-06-10 20:10:06 -0300

    upload: radio buttons and button convert

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,46 +1,83 @@
 import React, { useState } from 'react';
 import TextInput from '../TextInput'
+import Radio from '../Radio'
 import { makeStyles } from '@material-ui/core/styles';
+import SyncAltIcon from '@material-ui/icons/SyncAlt';
 
 function Form(props) {
     const classes = useStyles();
     const [dolar, setDolar] = useState('0')
     const [tax, setTax] = useState('0')
+    const [typeBuy, setTypeBuy] = useState('money')
+
+    const listOfTypesBuy = [
+        { value: 'money', label: 'Dinheiro' },
+        { value: 'card', label: 'Cartão' }
+    ];
+
     return (
         <div>
             <div className={classes.label}>
-                <TextInput 
-                    label="Dólar" 
-                    value={dolar} 
-                    setValue={setDolar} 
-                    money 
+                <TextInput
+                    label="Dólar"
+                    value={dolar}
+                    setValue={setDolar}
+                    money
                 />
-                <TextInput 
-                    label="Taxa do Estado" 
-                    value={tax} 
-                    setValue={setTax} 
+                <TextInput
+                    label="Taxa do Estado"
+                    value={tax}
+                    setValue={setTax}
                     percent
+                />
+            </div>
+            <div style={{ paddingTop: 15 }}>
+                <Radio
+                    label="Tipo de compra"
+                    list={listOfTypesBuy}
+                    select={typeBuy}
+                    setSelect={setTypeBuy}
                 />
             </div>
             <div
                 onClick={() => props.setPageForm(false)}
                 className={classes.navButton}
             >
-                Ir para resultado
+                <div className={classes.fontButton}>
+                    <SyncAltIcon />
+                    Converter
+                </div>
             </div>
         </div >
     );
 }
 
 const useStyles = makeStyles({
-    navButton: { 
-        paddingTop: 10, 
-        textDecoration: 'underline', 
-        cursor: 'pointer' 
+    navButton: {
+        border: '1px solid #008B57',
+        borderRadius: 8,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 27,
+        width: 149,
+        height: 56,
+        boxShadow: '0px 8px 4px rgba(13, 17, 27, 0.08)',
+        backgroundColor: '#00AB63',
+        cursor: 'pointer'
     },
-    label: { 
-        display: 'flex', 
-        flexDirection: 'row' 
+    label: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    fontButton: {
+        width: '100%',
+        fontSize: 16,
+        fontFamily: 'Noto Sans JP',
+        color: '#FFF',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
     }
 });
 
---
+++ src/Components/Radio/index.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+function Radio(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.label}>
+                {props.label}
+            </div>
+            <div className={classes.content}>
+                {props.list.map(item => (
+                    <div
+                        className={classes.radio} onClick={() => props.setSelect(item.value)}>
+                        <div className={
+                            props.select === item.value ?
+                                classes.radioButtonSelected
+                                :
+                                classes.radioButton
+                        }>
+                            {props.select === item.value ?
+                                <div className={classes.check} />
+                                :
+                                false
+                            }
+                        </div>
+                        <div className={classes.radioText}>{item.label}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+const useStyles = makeStyles({
+    container: {
+        padding: '16px 16px 0px 0px'
+    },
+    label: {
+        fontFamily: 'Roboto',
+        paddingBottom: 16,
+        fontWeight: 600,
+        fontSize: 18
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    radio: {
+        cursor: 'pointer',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingRight: 20
+    },
+    radioButtonSelected: {
+        marginRight: 5,
+        border: '2px solid #008B57',
+        width: 18,
+        height: 18,
+        borderRadius: 18,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    radioButton: {
+        marginRight: 5,
+        border: '2px solid #8C9CAD',
+        width: 18,
+        height: 18,
+        borderRadius: 18,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    check: {
+        backgroundColor: '#008B57',
+        width: 12,
+        height: 12,
+        borderRadius: 12
+    },
+    radioText: {
+        fontFamily: 'Roboto',
+        fontWeight: '590',
+        fontSize: 16,
+    }
+});
+
+export default Radio;
